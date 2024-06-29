070a6f4b0e9e505a66330909a436ae19fad24d4b
Harsha <harshahm0130@gmail.com>
2024-06-29 14:22:06 +0530

    Fetching weather data

--- main.py
+++ main.py
@@ -6,10 +6,22 @@ api_key = "7a941c35c1609e319e27bd456ccf9cf0"
 weather_params = {
     "lat": 12.971599,
     "lon": 77.594566,
-    "apiid": api_key
+    "appid": api_key,
+    "cnt": 4
 }
 
 response = requests.get(OWM_Endpoint, params=weather_params)
-# print(response.status_code)
-print(response.json())
+response.raise_for_status()
+weather_data = response.json()
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    print(condition_code)
+
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Carry Umbrella")
 
